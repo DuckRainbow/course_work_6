b983aac01b264187b6949b87a8883b231422343f
DuckRainbow <sillestyna@gmail.com>
2024-06-26 16:53:23 +0500

    mail try model

--- mail/models.py
+++ mail/models.py
@@ -77,3 +77,61 @@ class Mail(models.Model):
     class Meta:
         verbose_name = "рассылка"
         verbose_name_plural = "рассылки"
+
+
+class MailMessage(models.Model):
+    subject = models.CharField(
+        max_length=100,
+        verbose_name="Тема письма",
+        blank=True,
+        null=True,
+        help_text='Введите тему письма.'
+    )
+    body = models.TextField(
+        verbose_name="Тело письма",
+        blank=True,
+        null=True,
+        help_text='Введите текст письма.'
+    )
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f"{self.subject}"
+
+    class Meta:
+        verbose_name = "сообщение"
+        verbose_name_plural = "сообщения"
+
+
+class MailTry(models.Model):
+    date_time = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата и время последней попытки",
+        blank=True,
+        null=True
+    )
+    TRY_STATUS = (
+        ('success', 'Успешно'),
+        ('fail', 'Не успешно'),
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=TRY_STATUS,
+        verbose_name="Cтатус попытки",
+        blank=True,
+        null=True
+    )
+    servers_answer = models.CharField(
+        max_length=100,
+        verbose_name="Ответ сервера",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f"{self.date_time}, {self.status}"
+
+    class Meta:
+        verbose_name = "попытка"
+        verbose_name_plural = "попытки"
