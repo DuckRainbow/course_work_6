ad1fca2f2f554b58cffec79dc024a8090378d0e8
DuckRainbow <sillestyna@gmail.com>
2024-06-28 21:27:54 +0500

    forms, views alfa ver

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
+class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Email')
     token = models.CharField(
         max_length=100,
