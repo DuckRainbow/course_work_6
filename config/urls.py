7fadb426bef546b3ac8f67ad4d3eb6ba8cd85a86
DuckRainbow <sillestyna@gmail.com>
2024-07-02 20:43:19 +0500

    всё

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,6 @@ from django.urls import path, include
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('mail.urls', namespace='mail')),
+                  path('', include('blog.urls', namespace='blog')),
                   path('users/', include('users.urls', namespace='users')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
