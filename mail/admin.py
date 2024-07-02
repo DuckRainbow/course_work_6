3f50a3501c41cb35fd059fb1479c032a44713bb3
DuckRainbow <sillestyna@gmail.com>
2024-07-02 19:52:12 +0500

    всё

--- mail/admin.py
+++ mail/admin.py
@@ -6,20 +6,17 @@ from mail.models import Client, MailMessage, Mail
 # Register your models here.
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'email', 'owner')
-    list_filter = ('owner',)
-    search_fields = ('email', 'full_name', 'owner')
+    list_display = ('id', 'full_name', 'email')
+    search_fields = ('email', 'full_name')
 
 
 @admin.register(MailMessage)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'subject', 'body', 'owner')
-    list_filter = ('owner',)
-    search_fields = ('subject', 'owner')
+    list_display = ('id', 'subject', 'body')
 
 
 @admin.register(Mail)
 class MailAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'first_date', 'periodicity', 'status', 'mail_message', 'clients', 'owner')
-    list_filter = ('owner', 'clients', 'mail_message', 'status', 'periodicity')
-    search_fields = ('title', 'first_date', 'owner')
+    list_display = ('id', 'title', 'first_date', 'periodicity', 'status')
+    list_filter = ('status', 'periodicity')
+    search_fields = ('title', 'first_date')
