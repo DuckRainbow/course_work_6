0562f0d422ceadf0f4acfa1e653277153f56aecf
DuckRainbow <sillestyna@gmail.com>
2024-07-02 20:44:32 +0500

    всё

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-# from config import settings
-
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('mail.urls', namespace='mail')),
