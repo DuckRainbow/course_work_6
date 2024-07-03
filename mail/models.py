a8d5f30f8d13d42caa251ab59cb139bfe55cac27
DuckRainbow <sillestyna@gmail.com>
2024-07-03 21:17:45 +0500

    всё

--- mail/models.py
+++ mail/models.py
@@ -82,8 +82,8 @@ class Mail(models.Model):
         null=True,
         help_text='Введите название рассылки.'
     )
-    first_date = models.DateTimeField(
-        verbose_name="Дата первой отправкм",
+    first_date = models.DateField(
+        verbose_name="Дата первой отправки",
         blank=True,
         null=True
     )
